15a61d694ba26d79417df84eb92f7c251e2a0577
harshilmehta <harshil.mehta@vphrase.com>
2023-10-08 11:44:22 +0530

    CHORE: Rendering chart in card.

--- frontend/src/components/AnalyticsDashboard.vue
+++ frontend/src/components/AnalyticsDashboard.vue
@@ -3,36 +3,36 @@
     <vs-row>
       <period-filter
         @update-time-filter="handleFilterChange"
-      ></period-filter>
+      />
     </vs-row>
     <vs-row
       vs-w="12"
       style="margin-bottom: 25px"
     >
-       <vs-col vs-offset="0.5" vs-type="flex" vs-align="flex-start" vs-w="12">
-          OVERVIEW
-       </vs-col>
+      <vs-col vs-offset="0.5" vs-type="flex" vs-align="flex-start" vs-w="12">
+        OVERVIEW
+      </vs-col>
     </vs-row>
     <vs-row
       vs-w="11.5"
     >
       <vs-col vs-offset="0.5" vs-type="flex" vs-align="flex-start" vs-w="12">
-      <OverviewCards
-      :card-data="tiles"
-      ></OverviewCards>
+        <OverviewCards
+          :card-data="tiles"
+        />
       </vs-col>
     </vs-row>
 
     <div
-    style="margin-bottom: 50px;"
+      style="margin-bottom: 50px;"
     ></div>
     <vs-row
       vs-w="12"
       style="margin-bottom: 25px"
     >
-       <vs-col vs-offset="0.5" vs-type="flex" vs-align="flex-start" vs-w="12">
-          ACTIVITIES
-       </vs-col>
+      <vs-col vs-offset="0.5" vs-type="flex" vs-align="flex-start" vs-w="12">
+        ACTIVITIES
+      </vs-col>
     </vs-row>
     <vs-row
       vs-w="11.5"
@@ -42,9 +42,11 @@
         <div
           class="chart"
         >
-          <app-chart
-          :chart="chart"
-          />
+          <vs-card>
+            <app-chart
+              :chart="chart"
+            />
+          </vs-card>
         </div>
       </vs-col>
     </vs-row>
@@ -52,34 +54,35 @@
 </template>
 
 <script>
-import OverviewCards from '@/components/OverviewCards';
-import AppChart from '@/components/AppChart';
-import PeriodFilter from '@/components/PeriodFilter';
-export default {
-  name: "AnalyticsDashboard",
-  components: { PeriodFilter, AppChart, OverviewCards },
-  data () {
-    return {
-      tiles:[],
-      chart:{}
-    }
-  },
-  beforeMount: function () {
-    this.getAnalytics();
-  },
-  methods:{
-    handleFilterChange: function (payload){
-      this.getAnalytics(payload);
+  import OverviewCards from '@/components/OverviewCards';
+  import AppChart from '@/components/AppChart';
+  import PeriodFilter from '@/components/PeriodFilter';
+
+  export default {
+    name: 'AnalyticsDashboard',
+    components: { PeriodFilter, AppChart, OverviewCards },
+    data () {
+      return {
+        tiles: [],
+        chart: {}
+      };
     },
-    getAnalytics: function (payload){
-      if (!payload){
-        payload = {};
-        payload['time_period'] = '24_hours';
-      }
-      this.axiosPromise({
-        url: '/dashboard/',
-        method: 'GET',
-        queryParams:payload
+    beforeMount: function () {
+      this.getAnalytics();
+    },
+    methods: {
+      handleFilterChange: function (payload) {
+        this.getAnalytics(payload);
+      },
+      getAnalytics: function (payload) {
+        if (! payload) {
+          payload = {};
+          payload['time_period'] = '24_hours';
+        }
+        this.axiosPromise({
+          url: '/dashboard/',
+          method: 'GET',
+          queryParams: payload
         }).then(response => {
           this.tiles = response['tile'];
           this.chart = response['chart'];
@@ -88,7 +91,7 @@ export default {
         });
       }
     }
-};
+  };
 </script>
 
 <style scoped>
